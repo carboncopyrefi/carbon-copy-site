5d43561f2bf01d1c55953e1645bd3198edbcd1bf
kentbabin <58686361+kentbabin@users.noreply.github.com>
2023-07-23 19:20:48 +0800

    added client side only for project news

--- components/ProjectNews.vue
+++ components/ProjectNews.vue
@@ -1,21 +1,28 @@
 <script setup>
 
   const { pending, data } = await useFetch('https://carboncopy.pythonanywhere.com/news', {
-    lazy: true,
-    server: false
+    server: false,
+    lazy: true
   })
 
 </script>
 
 <template>
-  <div v-if="data" class="position-sticky" style="top: 2rem;">
+  <div class="position-sticky" style="top: 2rem;">
     <div class="p-4 mb-3 bg-light rounded">
       <h2 class="pb-4 mb-1">Project News</h2>
-      <div v-for="item in data" class="border-bottom pb-4 mb-3">
-        <p><small class="text-body-secondary">{{ item.date }}</small></p>
-        <p class="fw-bold article-title">{{ item.title }}</p>
-        <p class="article-description">{{ item.snippet }}</p>
-        <NuxtLink :to="item.link" target="_blank">Read more</NuxtLink>
+      <div v-if="pending" class="d-flex justify-content-center mt-5">
+        <div class="spinner-border text-primary" role="status">
+          <span class="visually-hidden">Loading project news...</span>
+        </div>
+      </div>
+      <div v-if="data">
+        <div v-for="item in data" class="border-bottom pb-4 mb-3">
+          <p><small class="text-body-secondary">{{ item.date }}</small></p>
+          <p class="fw-bold article-title">{{ item.title }}</p>
+          <p class="article-description">{{ item.snippet }}</p>
+          <NuxtLink :to="item.link" target="_blank">Read more</NuxtLink>
+        </div>
       </div>
     </div>
   </div>
