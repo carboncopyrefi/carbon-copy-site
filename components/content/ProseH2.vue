253fb8da2a13d24efd8b96dc44adb86008561436
kentbabin <58686361+kentbabin@users.noreply.github.com>
2023-09-15 21:50:33 +0800

    updates
    - added new article on tokenised credits
    - updated packages
    - added custom H2 and H4 content components

---
+++ components/content/ProseH2.vue
@@ -0,0 +1,13 @@
+<template>
+  <h2 :id="id" class="mb-3 mt-4">
+      <slot />
+  </h2>
+</template>
+
+<script setup lang="ts">
+  import { computed, useRuntimeConfig } from '#imports'
+
+  const props = defineProps<{ id?: string }>()
+
+  const { headings } = useRuntimeConfig().public.mdc
+</script>
---
+++ components/content/ProseH4.vue
@@ -0,0 +1,13 @@
+<template>
+  <h4 :id="id">
+    <slot />
+  </h4>
+</template>
+
+<script setup lang="ts">
+  import { computed, useRuntimeConfig } from '#imports'
+
+  const props = defineProps<{ id?: string }>()
+
+  const { headings } = useRuntimeConfig().public.mdc
+</script>
