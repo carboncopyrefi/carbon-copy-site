2688cec89b3dcab207ff211d7b6436059f74c68a
kentbabin <58686361+kentbabin@users.noreply.github.com>
2023-10-05 08:19:25 +0800

    updates
    -made events client side rendering
    -added climate finance article

--- pages/events/index.vue
+++ pages/events/index.vue
@@ -1,6 +1,9 @@
 <script setup>
 
-const { data } = await useFetch('https://api.carboncopy.news/events')
+const { pending, data } = await useFetch('https://api.carboncopy.news/events', {
+  server: false,
+  lazy: true
+})
 // const { data } = await useFetch('http://127.0.0.1:5000/events')
 
   useHead({
@@ -11,9 +14,14 @@ const { data } = await useFetch('https://api.carboncopy.news/events')
 
 <template>
 
-<h1 class="mt-5 mb-3">Upcoming ReFi Events</h1>
-
-<EventCard :data=data></EventCard>
-
+  <h1 class="mt-5 mb-3">Upcoming ReFi Events</h1>
+  <div v-if="pending" class="d-flex justify-content-center my-5">
+    <div class="spinner-border text-primary" role="status">
+      <span class="visually-hidden">Loading project news...</span>
+    </div>
+  </div>
+  <div v-if="data">
+    <EventCard :data=data></EventCard>
+  </div> 
 
 </template>
