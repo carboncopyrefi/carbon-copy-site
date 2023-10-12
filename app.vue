03bc9dae35f3d880085bef20a104f340f420d51b
kentbabin <58686361+kentbabin@users.noreply.github.com>
2023-10-12 17:16:17 +0800

    image link updates

--- app.vue
+++ app.vue
@@ -27,7 +27,7 @@ useSeoMeta({
   ogTitle: '%s | CARBON Copy',
   description: 'The preeminent source of ReFi news, information, and analysis.',
   ogDescription: 'The preeminent source of ReFi news, information, and analysis.',
-  ogImage: 'https://flat-term-5905.on.fleek.co/meta.jpg',
+  ogImage: 'https://carboncopy.news/meta.jpg',
   twitterCard: 'summary_large_image',
   ogType: 'website',
   ogUrl: 'https://carboncopy.news'
