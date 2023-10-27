3e3c4d640377261d204a282bc6825dd834ebe08a
kentbabin <58686361+kentbabin@users.noreply.github.com>
2023-10-27 17:44:09 +0800

    Update [slug].vue

--- pages/this-week-in-refi/[slug].vue
+++ pages/this-week-in-refi/[slug].vue
@@ -13,7 +13,7 @@
     meta: [
       { hid: 'og:image', property:'og:image', content: 'https://carboncopy.news' + img(data.value?.mainImage, { width: 800, quality:80 }) },
       { hid: 'twitter:description', name: 'twitter:description', content: data.value?.description },
-      { hid: 'twitter:title', name: 'twitter:title', content: 'This Week in ReFi:' + data.value?.title },
+      { hid: 'twitter:title', name: 'twitter:title', content: 'This Week in ReFi: ' + data.value?.title },
       { hid: 'twitter:image', name: 'twitter:image', content: 'https://carboncopy.news' + img(data.value?.mainImage, { width: 800, quality:80})},
     ],
     script: [
