c5c336fddbd192e1a7db5f42f3548be0cd0f0a6e
kentbabin <58686361+kentbabin@users.noreply.github.com>
2023-11-01 17:41:05 +0800

    updates
    - added ACT learn article
    - updated packages

---
+++ components/content/ProseH3.vue
@@ -0,0 +1,13 @@
+<template>
+    <h3 :id="id">
+      <slot />
+    </h3>
+  </template>
+  
+  <script setup lang="ts">
+    import { computed, useRuntimeConfig } from '#imports'
+  
+    const props = defineProps<{ id?: string }>()
+  
+    const { headings } = useRuntimeConfig().public.mdc
+  </script>
