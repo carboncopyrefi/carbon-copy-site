67d3d93dd4a6bd2a72781544f3f47108281a3324
kentbabin <58686361+kentbabin@users.noreply.github.com>
2023-12-20 19:01:41 +0800

    added cop recap article

--- components/content/Editorial.vue
+++ components/content/Editorial.vue
@@ -1,5 +1,5 @@
 <template>
   <hr />
-  <em>This article represents the opinion of the CARBON Copy editor and does not necessarily reflect the views of CARBON Copy.</em>
+  <em>This article represents the opinion of the CARBON Copy editor and does not necessarily reflect the editorial stance of CARBON Copy.</em>
 
 </template>
