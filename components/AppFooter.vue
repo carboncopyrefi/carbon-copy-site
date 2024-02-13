3a962e9d7567926d8cfd7a93a40df49665d9009e
kentbabin <58686361+kentbabin@users.noreply.github.com>
2024-02-13 17:26:42 +0800

    updated discord link

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -34,7 +34,7 @@
             </li>
             <li class="nav-item py-3 py-lg-0">
               <NuxtLink class="link-secondary text-decoration-none" to="https://twitter.com/carboncopy_refi" target="_blank"><i class="bi bi-twitter-x fs-5"></i></NuxtLink>&nbsp;&nbsp;&nbsp;
-              <NuxtLink class="link-secondary text-decoration-none" to="https://discord.carboncopy.news" target="_blank"><i class="bi bi-discord fs-5"></i></NuxtLink>&nbsp;&nbsp;&nbsp;
+              <NuxtLink class="link-secondary text-decoration-none" to="https://discord.gg/53TpqNgPC5" target="_blank"><i class="bi bi-discord fs-5"></i></NuxtLink>&nbsp;&nbsp;&nbsp;
               <NuxtLink class="link-secondary text-decoration-none" to="/feed.xml" target="_blank"><i class="bi bi-rss-fill fs-5"></i></NuxtLink>
             </li>
           </ul>
