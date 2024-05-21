891621fa44269760205021e83d4348eed7fb35a4
kentbabin <58686361+kentbabin@users.noreply.github.com>
2024-05-21 15:26:10 +0300

    added medici effect article

--- pages/features/[slug].vue
+++ pages/features/[slug].vue
@@ -13,6 +13,12 @@
       { hid: 'og:image', property:'og:image', content: 'https://carboncopy.news' + img(data.value?.mainImage, { width: 800, quality:80 }) },
       { hid: 'twitter:description', name: 'twitter:description', content: data.value?.description },
       { hid: 'twitter:image', name: 'twitter:image', content: 'https://carboncopy.news' + img(data.value?.mainImage, { width: 800, quality:80})},
+    ],
+    script: [
+        {
+            src: "https://platform.twitter.com/widgets.js",
+            async: "true"
+        }
     ]
   })
 
