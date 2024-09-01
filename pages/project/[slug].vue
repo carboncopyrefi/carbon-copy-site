4c3926f31531ae0c32b2680ff2bfdf4eb1aa64ea
kentbabin <58686361+kentbabin@users.noreply.github.com>
2024-09-01 09:44:22 +0300

    updates
    -added gg21 climate and shell rounds
    -added weekly recap to feed

--- pages/project/[slug].vue
+++ pages/project/[slug].vue
@@ -74,7 +74,7 @@ useHead({
         </div> -->
         <div class="row mb-5">
           <div class="col-lg-8">
-            <h2>Fundraising <Modal class="fs-5" id="fundraising_list" title="Grant Data" body="Our dataset includes the following grant rounds:<br><br>Climate Round (Gitcoin Grants Beta, 18, 19, 20)<br>Shell Round (Gitcoin Grants 19, 20)<br>Web3 Open Source Software (GG Beta, 18, 19, 20)<br>Web3 Community & Education (GG Beta, 18, 19)<br>Celo QF Round (Gitcoin Grants 20)<br>CeloRPGF0<br>RetroPGF (Round 4)<br>Octant Community Fund<br>Giveth Cumulative (QF matching + donations)"></Modal></h2>
+            <h2>Fundraising <Modal class="fs-5" id="fundraising_list" title="Grant Data" body="Our dataset includes the following grant rounds:<br><br>Climate Round (Gitcoin Grants Beta, 18, 19, 20, 21)<br>Shell Round (Gitcoin Grants 19, 20, 21)<br>Web3 Open Source Software (GG Beta, 18, 19, 20)<br>Web3 Community & Education (GG Beta, 18, 19)<br>Celo QF Round (Gitcoin Grants 20)<br>CeloRPGF0<br>RetroPGF (Round 4)<br>Octant Community Fund<br>Giveth Cumulative (QF matching + donations)"></Modal></h2>
             <span v-if="status === 'pending'" class="d-flex justify-content-center my-5">
               <div class="spinner-border text-primary" role="status1">
                 <span class="visually-hidden">Loading fundraising data...</span>
