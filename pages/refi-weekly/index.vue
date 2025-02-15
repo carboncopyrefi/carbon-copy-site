92461c89e3b25acc6250706b191af6ad73fddb6a
kentbabin <58686361+kentbabin@users.noreply.github.com>
2025-02-15 11:54:57 +0800

    Update index.vue

--- pages/refi-weekly/index.vue
+++ pages/refi-weekly/index.vue
@@ -52,29 +52,30 @@
     </div>
     <div class="row mt-5">
         <h2>All Episodes</h2>
-    
-        <ClientOnly>
-            <div v-if="status === 'pending'" class="d-flex justify-content-center my-5">
-                <div class="spinner-border text-primary" role="status">
-                <span class="visually-hidden">Loading ReFi Weekly episodes...</span>
+        <div class="col-12">
+            <ClientOnly>
+                <div v-if="status === 'pending'" class="d-flex justify-content-center my-5">
+                    <div class="spinner-border text-primary" role="status">
+                    <span class="visually-hidden">Loading ReFi Weekly episodes...</span>
+                    </div>
                 </div>
-            </div>
-            <div v-if="data" >
-                <div v-for="episode in data" :key="episode.id">
-                    <div class="col-lg-6">
-                        <div class="card shadow-sm">
+                <div v-if="data" class="row">
+                    <div v-for="episode in data" :key="episode.id" class="col-lg-6 mb-3">
+                        <div class="card shadow-sm h-100">
                             <div class="card-body">
                                 <h4>{{ episode.title }}</h4>
                                 <p v-html=episode.subtitle></p>
                                 <p>{{ episode.date }}</p>
+                            </div>
+                            <div class="card-footer border-0 bg-white pb-3">
                                 <NuxtLink external :href="episode.link" class="btn btn-primary me-3" target="_blank">Listen on X</NuxtLink>
                                 <NuxtLink :to="'/refi-weekly/' + episode.slug + '/'" class="btn btn-success">Read Summary</NuxtLink>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </ClientOnly>
-    </div>
+            </ClientOnly>
+        </div>
+    </div>  
 
 </template> 
