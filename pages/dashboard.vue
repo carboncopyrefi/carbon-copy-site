598f7e59a69123072eecce55a8a400c355090780
kentbabin <58686361+kentbabin@users.noreply.github.com>
2025-03-24 20:15:22 +0800

    Update dashboard.vue

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -226,7 +226,7 @@ useHead({
 
     <div class="row mt-5">
       <ClientOnly>
-        <p class="small">** Data current as of March 16th, 2025. **</p>
+        <p class="small">** Data current as of March 24th, 2025. **</p>
       </ClientOnly>
     </div>
 </template>
