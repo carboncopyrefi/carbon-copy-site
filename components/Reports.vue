ce5eb0a8509cfccb26e38dc7d5ab826ca5e2f9cd
kentbabin <58686361+kentbabin@users.noreply.github.com>
2025-05-28 21:37:16 +0800

    Update Reports.vue

--- components/Reports.vue
+++ components/Reports.vue
@@ -4,7 +4,25 @@
     </h2>
     <div class="row mb-5">
         <div class="col-md-6">
-            <div class="card mb-3 bg-light border border-0 rounded-0">
+            <div class="card mb-3 bg-light border border-0 rounded-0 h-100">
+                <div class="row g-0 h-100">
+                    <div class="col-lg-4">
+                        <NuxtImg src="/images/state-of-refi-report-cover-2025-portrait.jpg" class="d-none d-md-block" style="object-fit: cover;height:100%; width:100%;" alt="The State of ReFi Report 2025 Cover" format="webp" loading="lazy" />
+                    </div>
+                    <div class="col-lg-8">
+                        <div class="card-body">
+                            <p class="lead fw-bold">The State of ReFi Report 2025</p>
+                            <p>Digging deeper into the Web3 Regenerative Finance (ReFi) movement and its potential for ecological and social impact.</p>
+                        </div>
+                        <div class="card-footer bg-light border border-0 mt-xxl-5 mt-md-5">
+                            <NuxtLink to="/reports/The State of ReFi Report 2025.pdf" target="_blank" class="btn btn-primary btn-lg w-100">Download</NuxtLink>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card mb-3 bg-light border border-0 rounded-0 h-100">
                 <div class="row g-0">
                     <div class="col-lg-4">
                         <NuxtImg src="/images/impact-depin-report-cover.jpg" class="d-none d-md-block"  style="object-fit: cover;height:100%; width:100%;" alt="The Impact DePIN Report Cover" format="webp" loading="lazy" />
@@ -16,24 +34,6 @@
                         </div>
                         <div class="card-footer bg-light border border-0 mt-xxl-5 mt-md-5">
                             <NuxtLink to="/reports/The Impact DePIN Report 2024.pdf" target="_blank" class="btn btn-primary btn-lg w-100">Download</NuxtLink>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-6">
-            <div class="card mb-3 bg-light border border-0 rounded-0">
-                <div class="row g-0 h-100">
-                    <div class="col-lg-4">
-                        <NuxtImg src="/images/state-of-refi-report-cover.jpg" class="d-none d-md-block" style="object-fit: cover;height:100%; width:100%;" alt="The State of ReFi Report Cover" format="webp" loading="lazy" />
-                    </div>
-                    <div class="col-lg-8">
-                        <div class="card-body">
-                            <p class="lead fw-bold">The State of ReFi Report 2024</p>
-                            <p>Digging deeper into the Web3 Regenerative Finance (ReFi) movement and its potential for ecological and social impact.</p>
-                        </div>
-                        <div class="card-footer bg-light border border-0 mt-xxl-5 mt-md-5">
-                            <NuxtLink to="/reports/The State of ReFi Report 2024.pdf" target="_blank" class="btn btn-primary btn-lg w-100">Download</NuxtLink>
                         </div>
                     </div>
                 </div>
